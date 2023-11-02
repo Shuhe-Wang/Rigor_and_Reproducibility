d3a850bab401cf6e5c59a0f48a4ff701d2752b34
Shuhe-Wang <138421834+Shuhe-Wang@users.noreply.github.com>
2023-11-02 13:37:58 -0400

    Add files via upload

---
+++ HW7.py
@@ -0,0 +1,8 @@
+print("2023-11-02")
+print("Result of 2*5:", 2*5)
+
+# Write a reflection on how using repositories, tracking changes and publishing likt GitHib can help with computational reproducibility.
+
+# I think GitHib is very useful in storing and sharing codes. By making codes related to a publication public, it makes it easier for readers to verify the results and learn from the technique. It also benefits the reserachers by traching all the historical changes. This makes it easier to collaborate in a tream.
+
+# It's also necessary to keep track of the computational environment used to run the codes. It improves the reproducibility of scientific works.
